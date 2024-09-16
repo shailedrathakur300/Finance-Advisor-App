4d6903a6a9787a2b125c723eba75d5485c6f328e
shailendrathakur300 <shailendrathakuracademic123@gmail.com>
2024-09-16 10:29:43 +0530

    scroll-container from accernityui

---
+++ src/app/_components/Hero.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Image from "next/image";
+import { ContainerScroll } from "../../components/ui/container-scroll-animation";
+
+export default function Hero() {
+  return (
+    <section className="bg-gray-50 flex items-center  flex-col">
+      <div className="flex flex-col overflow-hidden">
+        <ContainerScroll
+          titleComponent={
+            <>
+              <h1 className="text-4xl font-semibold text-black dark:text-white">
+                Unleash the power of <br />
+                <span className="text-4xl md:text-[6rem] font-bold mt-1 leading-none">
+                  Scroll Animations
+                </span>
+              </h1>
+            </>
+          }
+        >
+          <Image
+            src="/dashboard.png"
+            alt="hero"
+            height={720}
+            width={1400}
+            className="mx-auto rounded-2xl object-cover h-full object-left-top"
+            draggable={false}
+          />
+        </ContainerScroll>
+      </div>
+    </section>
+  );
+}
